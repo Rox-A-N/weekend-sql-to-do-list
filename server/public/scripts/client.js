eace78fbd7c305ce2fb77c42e3831d90a84fb069
Roxanne Alvidrez Niederkorn <rox.niederkorn@gmail.com>
2022-12-17 01:05:20 -0600

    initial add

---
+++ server/public/scripts/client.js
@@ -0,0 +1,7 @@
+console.log('in the client.js');
+
+$(readyNow);
+
+function readyNow() {
+
+} // end readyNow
---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express'); // setting up express
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+// Serve "static assets" (html, css, client-side js)
+// from the server/public folder
+app.use(express.static('server/public'));
+
+// start express
+const port = 5001;
+app.listen(port, () => {
+    console.log('listening on port ', port);
+});
