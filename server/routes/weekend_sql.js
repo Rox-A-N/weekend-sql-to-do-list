ba02d194edfa73604c6a6aad951d8204c8325944
Roxanne Alvidrez Niederkorn <rox.niederkorn@gmail.com>
2022-12-21 01:22:57 -0600

    updated const pool in weekend_sql

--- server/routes/weekend_sql.js
+++ server/routes/weekend_sql.js
@@ -1,24 +1,6 @@
 const express = require( 'express' );
 const router = express.Router();
-const pg = require( 'pg' );
-
-const Pool = pg.Pool;
-
-const pool = new Pool({
-    database: 'weekend-to-do-app',
-    host: 'localhost',
-    port: 5432,
-    max: 10,
-    idleTimeoutMillis: 30000
-});
-
-pool.on('connect', () => {
-    console.log('postgres is connected');
-});
-
-pool.on( 'error', (error) => {
-    console.log('error! postgres NOT connected', error);
-});
+const pool = require('../modules/pool');
 
 // get data
 router.get('/', (req, res) => {
